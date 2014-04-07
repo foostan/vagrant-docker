0bf1dd111544e9017a1a52c0650ba7fb9f5aecf8
foostan <foostan27@gmail.com>
2014-04-08 02:14:39 +0900

    Update handler.sh

--- serf/handle.sh
+++ serf/handle.sh
@@ -1,22 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
-echo
-echo "$0 triggered!"
-echo
-echo "SERF_EVENT is ${SERF_EVENT}"
-echo "SERF_SELF_NAME is ${SERF_SELF_NAME}"
-echo "SERF_SELF_ROLE is ${SERF_SELF_ROLE}"
-echo "SERF_SELF_TAG is ${SERF_SELF_TAG}"
-echo "SERF_TAG_ROLE is ${SERF_TAG_ROLE}"
-echo "SERF_USER_EVENT is ${SERF_USER_EVENT}"
-echo "SERF_USER_LTIME is ${SERF_USER_LTIME}"
-echo "SERF_QUERY_NAME is ${SERF_QUERY_NAME}"
-echo "SERF_QUERY_LTIME is ${SERF_QUERY_LTIME}"
-echo
-echo "BEGIN event data"
+echo "SERF_EVENT:       ${SERF_EVENT}"
+echo "SERF_SELF_NAME:   ${SERF_SELF_NAME}"
+echo "SERF_SELF_ROLE:   ${SERF_SELF_ROLE}"
+echo "SERF_SELF_TAG:    ${SERF_SELF_TAG}"
+echo "SERF_TAG_ROLE:    ${SERF_TAG_ROLE}"
+echo "SERF_USER_EVENT:  ${SERF_USER_EVENT}"
+echo "SERF_USER_LTIME:  ${SERF_USER_LTIME}"
+echo "SERF_QUERY_NAME:  ${SERF_QUERY_NAME}"
+echo "SERF_QUERY_LTIME: ${SERF_QUERY_LTIME}"
 while read line; do
-  echo $line
+  echo "event data:       ${line}"
 done
-echo "END event data"
-echo "$0 finished!"
-echo
